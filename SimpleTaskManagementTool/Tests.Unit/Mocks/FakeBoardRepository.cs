0f4d0a7978fe7288c5a6b19f1f8ee44fbfa38936
Andriy Shcherbak <44274638+AndreyScherbak@users.noreply.github.com>
2025-06-05 21:34:45 +0300

    Add comprehensive unit tests

--- SimpleTaskManagementTool/Tests.Unit/Mocks/FakeBoardRepository.cs
+++ SimpleTaskManagementTool/Tests.Unit/Mocks/FakeBoardRepository.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
 
-namespace Tests.Unit.Mocks
+namespace Tests.Unit.Mocks;
+
+internal sealed class FakeBoardRepository : IBoardRepository
 {
-    internal class FakeBoardRepository
-    {
-    }
+    private readonly List<Board> _boards = new();
+
+    public IReadOnlyList<Board> Boards => _boards;
+
+    public void Add(Board board) => _boards.Add(board);
+
+    public Task<Board?> GetByIdAsync(Guid id, CancellationToken ct = default)
+        => Task.FromResult<Board?>(_boards.FirstOrDefault(b => b.Id == id));
+
+    public Task<Board?> GetBoardWithTaskAsync(Guid boardId, Guid taskId, CancellationToken ct = default)
+        => Task.FromResult<Board?>(
+            _boards.FirstOrDefault(b => b.Id == boardId && b.Tasks.Any(t => t.Id == taskId)));
+
+    public Task<IReadOnlyList<Board>> GetAllByUserIdAsync(Guid userId, CancellationToken ct = default)
+        => Task.FromResult((IReadOnlyList<Board>)_boards.ToList());
+
+    public Task SaveChangesAsync(CancellationToken ct = default) => Task.CompletedTask;
+
+    public Task<bool> ExistsByTitleAsync(string title, CancellationToken ct = default)
+        => Task.FromResult(_boards.Any(b => b.Title == title));
 }
